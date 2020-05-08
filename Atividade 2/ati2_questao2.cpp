41474ac261f04f01d0d395fbe51e932943fad9f2
Mateus Santos <m.santos.azeve@gmail.com>
2020-05-07 22:01:44 -0300

    questão 2 lista 2

---
+++ Atividade 2/ati2_questao2.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<locale.h>
+
+main(){
+	int rep, cont, numero[101], avaliador = 0; setlocale(LC_ALL,"");
+	
+	printf("Quantas vezes quer testar? (Limite de 100 vezes): "); scanf("%d", &rep);
+	
+	for(cont = 1; cont <= rep; cont++){
+		system("cls");
+		puts("Para sair digite qualquer número negativo");
+		printf("Valor do Número %d: ", cont); scanf("%d", &numero[cont]);
+				
+		if(numero[cont] >= 0){
+			if(numero[cont] % 3 == 0){
+				avaliador++;
+			}
+		}
+		else{
+			break;
+		}
+	}
+	printf("\n%d números são multiplos de 3", avaliador);
+}
