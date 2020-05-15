7da2109c1c40fb8e4bac751b73cca038ee97afa9
Mateus Santos <m.santos.azeve@gmail.com>
2020-05-15 01:50:24 -0300

    função furada

--- Atividade 2/questao6.cpp
+++ Atividade 2/questao6.cpp
@@ -1,11 +1,27 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int num_menor(int x);
+
 main(){
-	int number;
+	int number, menor;
 	
 	do
 	{
 		printf("Teste: "); scanf("%d", &number);
+		
+		menor = num_menor(number);
+		
+		printf("Menor numero: %d\n", menor);
+		
 	}while(number != 0);
+}
+
+int num_menor(int x){
+	int menor; 
+	
+	if(x < menor){
+		menor =  x;
+	}
+	return menor;
 }
