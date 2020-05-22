2d188bb7c444642e0fbc46685c57b59bfdf6c8f2
Mateus Santos <m.santos.azeve@gmail.com>
2020-05-22 20:34:13 -0300

    Update Program questao6.cpp

--- Atividade 2/questao6.cpp
+++ Atividade 2/questao6.cpp
@@ -1,48 +1,66 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<locale.h>
 
 int number, menor = INT_MAX, maior = INT_MIN, soma = 0, media;
 
-int num_menor(int x){
-	if(x < menor){
+ int num_menor(int x){
+ 	if(x < menor){
 		menor =  x;
 	}
-	return menor;
+		return menor;
 }
 
-int num_maior(int x){
+ int num_maior(int x){
 	if(x > maior){
 		maior = x;
 	}
-	return maior;
+		return maior;
 }
 
-int num_soma(int x){
+ int num_soma(int x){
 	soma += x;
 	
 	return soma;
 }
 
-int num_media(int x, int y){
+ int num_media(int x, int y){
 	media = x / y;
 	
 	return media;
 }
 
 main(){
-	int contador;
+	int contador; setlocale(LC_ALL,"");
 	
 	number = 1;
 
 	for(contador = 1; number != 0; contador += 1){
+		system("cls");		
+		printf("Digite um numero(Se deseja encerrar digite 0): "); scanf("%d", &number);
 		
-		printf("Teste: "); scanf("%d", &number);
+		if (number == 0)
+			printf("\nFoi Digitado o número 0, ele não será contabilizado e encerrará o programa\n");			
+		else{
+			menor = num_menor(number);
+			maior = num_maior(number);
+			soma = num_soma(number);
+			media = num_media(soma, contador);
+		}				
 		
-		menor = num_menor(number);
-		maior = num_maior(number);
-		soma = num_soma(number);
-		media = num_media(soma, contador);
+		if(number == 0){
+			if(contador == 1){
+				printf("\nMenor numero: INVALID VALUE\nMaior numero: INVALID VALUE\nSoma: INVALID VALUE\nMedia: INVALID VALUE\n\n");
+			}
+			else{
+				printf("\nMenor numero: %d\nMaior numero: %d\nSoma: %d\nMedia: %d\n\n", menor, maior, soma, media);
+			}
+		}		
+		else{
+			printf("\nMenor numero: %d\nMaior numero: %d\nSoma: %d\nMedia: %d\n\n", menor, maior, soma, media);
+		}
 		
-		printf("\nMenor numero: %d\nMaior numero: %d\nSoma: %d\nMedia: %d\n\n", menor, maior, soma, media);		
+		system("pause");			
 	}
 }
+
