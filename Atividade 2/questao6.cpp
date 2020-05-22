393ae0c1260b89ecfd32ff472fb7f723d4e48068
Mateus Santos <m.santos.azeve@gmail.com>
2020-05-22 16:50:34 -0300

    FINALMENTE - questao6.cpp

--- Atividade 2/questao6.cpp
+++ Atividade 2/questao6.cpp
@@ -1,27 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int num_menor(int x);
-
-main(){
-	int number, menor;
-	
-	do
-	{
-		printf("Teste: "); scanf("%d", &number);
-		
-		menor = num_menor(number);
-		
-		printf("Menor numero: %d\n", menor);
-		
-	}while(number != 0);
-}
+int number, menor = INT_MAX, maior = INT_MIN, soma = 0, media;
 
 int num_menor(int x){
-	int menor; 
-	
 	if(x < menor){
 		menor =  x;
 	}
 	return menor;
 }
+
+int num_maior(int x){
+	if(x > maior){
+		maior = x;
+	}
+	return maior;
+}
+
+int num_soma(int x){
+	soma += x;
+	
+	return soma;
+}
+
+int num_media(int x, int y){
+	media = x / y;
+	
+	return media;
+}
+
+main(){
+	int contador;
+	
+	number = 1;
+
+	for(contador = 1; number != 0; contador += 1){
+		
+		printf("Teste: "); scanf("%d", &number);
+		
+		menor = num_menor(number);
+		maior = num_maior(number);
+		soma = num_soma(number);
+		media = num_media(soma, contador);
+		
+		printf("\nMenor numero: %d\nMaior numero: %d\nSoma: %d\nMedia: %d\n\n", menor, maior, soma, media);		
+	}
+}
+
